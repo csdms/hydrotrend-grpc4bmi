c7259c46b73d8d8d4b3f91e24bec227ce888b72a
Mark Piper <mark.piper@colorado.edu>
2025-09-04 14:27:23 -0600

    Set up, but don't call, grpc4bmi server

---
+++ server/hydrotrend-grpc4bmi-server.cxx
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "bmi_hydrotrend.h"
+#include "bmi_grpc_server.h"
+
+
+int main(int argc, char *argv[])
+{
+    printf("BmiHydrotrend grpc4bmi server\n");
+
+    Bmi *model = (Bmi *) malloc(sizeof(Bmi));
+
+    register_bmi_hydrotrend(model);
+
+    {
+        char model_name[BMI_MAX_COMPONENT_NAME];
+        
+        model->get_component_name(model, model_name);
+        printf("%s\n", model_name);
+    }
+
+    free(model);
+    return 0;
+}
